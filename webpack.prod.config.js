581e2e7d9d18ccd27dc4778e7aa26c573e4efda5
Jeph Johnson <hello@jephjohnson.com>
2017-02-25 14:57:08 +1100

    Removed ExtractTextPlugin

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,7 +1,5 @@
 const path = require('path')
 const webpack = require('webpack')
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
 
 module.exports = {
   devtool: 'source-map',
@@ -36,24 +34,13 @@ module.exports = {
         loader: 'babel-loader',
         include: path.join(__dirname, 'src')
       },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
-      },
+      { test: /\.scss?$/,
+        loader: 'style-loader!css-loader!sass-loader',
+        include: path.join(__dirname, 'src', 'styles') },
       { test: /\.png$/,
-        loader: 'file' 
-      },
+        loader: 'file' },
       { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        loader: 'file'
-      }
+        loader: 'file'}
     ]
-  },
-  plugins: [
-    new ExtractTextPlugin({
-     filename: 'style.css'
-    })
-  ]
+  }
 }
